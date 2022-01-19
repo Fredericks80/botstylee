fd732386f59943143c0b55015afb1e55ee4a1e20
Fredericks80 <95603728+Fredericks80@users.noreply.github.com>
2022-01-19 12:56:30 +0700

    Update tebakbendera_answer.js

--- plugins/tebakbendera_answer.js
+++ plugins/tebakbendera_answer.js
@@ -11,7 +11,7 @@ handler.before = async function (m) {
         if (['.tbhint', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
-            await this.sendButton(m.chat, `*Benar!* +${this.tebakbendera[id][2]} XP`, 'BOTSTYLE', 'Tebak Bendera', '.tebakbendera', m)
+            await this.sendButton(m.chat, `*Benar!* +${this.tebakbendera[id][2]} XP`, 'Mangtaf', 'Tebak Bendera', '.tebakbendera', m)
             clearTimeout(this.tebakbendera[id][3])
             delete this.tebakbendera[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`Dikit Lagi!`)
